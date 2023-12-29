cd35cac454cbc6a8298c7e11bee3bb09b0442254
IMANE BOUTATA <imanebt6@gmail.com>
2023-12-29 18:36:19 +0100

    tracking

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import Acceuil from "./acceuil/acceuil";
 import Register from './register/Register';
 import Login from './signin/Login';
+import Tracking from "./acceuil/tracking";
 
 
 function App() {
   return (
     <div className="App">
-      <Acceuil/>
-      <Register/>
-      <Login/>
+     <Tracking/>
     </div>
   );
 }
