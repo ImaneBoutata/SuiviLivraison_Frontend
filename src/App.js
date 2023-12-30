d077138b893e2d4fdd67d3111da8c0d279907ab5
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2023-12-30 09:44:59 +0100

    findTrackingUser page deplace the navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Acceuil from "./acceuil/acceuil";
 import Register from './register/Register';
 import Login from './signin/Login';
+import ColisTracking from './UserComponent/ColisTracking';
+import Navbar from './Navbar/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Acceuil/>
-      <Register/>
-      <Login/>
+
+      <Navbar />
+      <br />
+      <br />
+      <ColisTracking />
     </div>
   );
 }
--- src/acceuil/acceuil.jsx
+++ src/acceuil/acceuil.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     MDBBtn, MDBCollapse,
     MDBContainer, MDBDropdown, MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle, MDBIcon, MDBInput,
@@ -6,6 +6,7 @@ import {
     MDBNavbarBrand, MDBNavbarItem, MDBNavbarLink, MDBNavbarNav, MDBNavbarToggler
 } from 'mdb-react-ui-kit';
 import background from '../images/backgroundLivraison.jpeg';
+import Navbar from '../Navbar/Navbar';
 function Acceuil() {
     const imageStyles = {
         width: '100vw',
@@ -20,30 +21,21 @@ function Acceuil() {
     const [value, setValue] = useState('');
     return (
         <div className="Acceuil">
-            <MDBNavbar dark bgColor='dark'>
-                <MDBContainer fluid className="d-flex justify-content-between align-items-center">
-                    <MDBNavbarBrand href='#'>Delivery Track</MDBNavbarBrand>
-                    <div className="d-flex gap-3"> {/* Adding gap between buttons */}
-                        <MDBBtn color="dark" style={{ backgroundColor: "transparent", border: '1px solid #FF8300' }}>Sign In</MDBBtn>
-                        <MDBBtn color="orange" style={{ backgroundColor: '#FF8300' }}>Sign Up</MDBBtn>
 
-                    </div>
-                </MDBContainer>
-            </MDBNavbar>
-
+            <Navbar />
             <img src={background} className='img-fluid shadow-4' style={imageStyles} alt='Background' />
 
             <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', width: '60%', minHeigh: '30rem' }}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <MDBInput
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    label='Entrer la réference de votre colis'
-                    id='controlledValue'
-                    type='text'
-                    style={{ color: '#fff', backgroundColor: 'rgba(255, 255, 255, 0.9)', border: '1px solid #fff' }}
-                />
-                <MDBBtn color="orange" style={{ backgroundColor: '#FF8300' }}>Suivre</MDBBtn>
+                    <MDBInput
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        label='Entrer la réference de votre colis'
+                        id='controlledValue'
+                        type='text'
+                        style={{ color: '#fff', backgroundColor: 'rgba(255, 255, 255, 0.9)', border: '1px solid #fff' }}
+                    />
+                    <MDBBtn color="orange" style={{ backgroundColor: '#FF8300' }}>Suivre</MDBBtn>
 
                 </div>
 
